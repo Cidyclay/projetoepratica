5c49b6bc8ba9bacfbf45c6655d8157ad43ee0aa7
AlefThiago <alefsilvadn@gmail.com>
2023-04-08 19:05:32 -0300

    layout  padrao

---
+++ componentes/navBar.php
@@ -0,0 +1,32 @@
+<?php 
+    function navBar ($texto) {
+        return "
+        <aside>
+            <nav>
+                <a href='conta.php'>
+                    <img id='imgNavBar' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh-Bn7-KSnRyMXgvNe72nE1qh9kTxrBHM2cQ&usqp=CAU' alt='Perfil'>
+                </a>
+
+                <a class='iconesNavBar' href='/'>
+                    <i class='icones material-symbols-outlined'>home</i>
+                </a>
+
+                <a class='iconesNavBar' href='comunidade.php'>
+                    <i class='icones material-symbols-outlined'>groups</i>
+                </a>
+
+                <a class='iconesNavBar' href='jogos.php'>
+                    <i class='icones material-symbols-outlined'>sports_esports</i>
+                </a>
+            </nav>        
+
+        <h1 id='texto'>$texto</h1>
+        
+        <div>
+            <a class='iconesNavBar'  href='conta.php'>
+                <i class='icones material-symbols-outlined'>person</i>
+            </a>
+        </div>
+    </aside>
+    ";
+    }
